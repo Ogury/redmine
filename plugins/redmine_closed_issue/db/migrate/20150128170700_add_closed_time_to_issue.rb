64c03348cc7b1e914fe1b0b078c6d57710f77972
Mathieu Desvé <mathieu.desve@me.com>
2015-01-30 11:58:13 +0000

    Remove plugin

--- plugins/redmine_closed_issue/db/migrate/20150128170700_add_closed_time_to_issue.rb
+++
@@ -1,9 +0,0 @@
-class AddClosedTimeToIssue < ActiveRecord::Migration
-  def self.up
-    add_column :issues, :closed_date, :datetime
-  end
-
-  def self.down
-    remove_column :issues, :closed_date
-  end
-end
